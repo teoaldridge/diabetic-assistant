baa128d3af80da0fe959b3602790f47825a180ab
Teodora Aldridge <teodora.aldridge@bjss.com>
2023-08-02 12:28:07 +0100

    style the home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
+import classes from "./Home.module.css";
 
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Diabetic Assistant</h1>
-      <h3>
-        Go to <Link to="/boluscalculator">Bolus Calculator</Link>
-      </h3>
-      <h3>
-        Go to <Link to="/foodsearch">Food Checker</Link>
-      </h3>
-      <h3>
-        Go to <Link to="/rotation">Injection/Pump Site Rotation</Link>
-      </h3>
+      <h1>DiaBuddy</h1>
+      <div className={classes.buttoncontainer}>
+        <Link to="/rotation" className={classes.link}>
+          <button className={classes.circlebutton}>Site Rotation</button>
+        </Link>
+        <Link to="/boluscalculator" className={classes.link}>
+          <button className={classes.circlebutton}>Bolus Calculator</button>
+        </Link>
+        <Link to="/foodchecker" className={classes.link}>
+          <button className={classes.circlebutton}>Food Checker</button>
+        </Link>
+      </div>
     </div>
   );
 };
