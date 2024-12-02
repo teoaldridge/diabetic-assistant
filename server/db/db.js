ed4d03cc3583433a215e1676b1289e19dc15f426
teddyaldridge <teodora_aldridge@yahoo.com>
2024-12-02 16:14:40 +0000

    add authentication back end

---
+++ server/db/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+const config = require("./config");
+
+const connectDB = async () => {
+  const pool = mysql.createPool(config);
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log({ error: err.message });
+      return;
+    }
+
+    console.log("Connected to MySQL database");
+
+    connection.release();
+  });
+};
+
+module.exports = connectDB;
